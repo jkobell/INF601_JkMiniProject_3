336c3c7a7b00d8bc290dc4d401e3ee746236746e
jk185256 <jk185256@ncr.com>
2022-10-17 12:37:45 -0500

    initialize_app_file

---
+++ flaskr/__init__.py
@@ -0,0 +1,29 @@
+import os
+
+from flask import Flask
+
+# app object is created from a Flask instance
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+    # checks if run as a test then load test config; if not, load app config
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:        
+        app.config.from_mapping(test_config)
+
+    # check if SQLite folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # dev only page -- remove
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
