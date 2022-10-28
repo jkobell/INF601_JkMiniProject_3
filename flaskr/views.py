dfeff05539dd90704bdc607417955c32b4b8c05e
jk185256 <jk185256@ncr.com>
2022-10-28 15:33:36 -0500

    clean_unused

--- flaskr/views.py
+++ flaskr/views.py
@@ -1,18 +1,10 @@
 # INF601 - Advanced Programming in Python
 # James Kobell
 # Mini Project 3
-import functools
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, render_template
 )
-from werkzeug.security import check_password_hash, generate_password_hash
-
-from flaskr.db import get_db
-
-from werkzeug.exceptions import abort
-
-from flaskr.auth import login_required
 
 bp = Blueprint('views', __name__, url_prefix='/views')
 
@@ -20,21 +12,3 @@ bp = Blueprint('views', __name__, url_prefix='/views')
 def index():
     #? check session
     return render_template('views/index.html')
-
-
-""" @bp.route('/grocery')
-def grocery():
-    #? check session
-    return render_template('views/grocery.html') """
-
-@bp.route('/household')
-def household():
-    #? check session
-    return render_template('views/household.html')
-
-@bp.route('/vehicle')
-def vehicle():
-    #? check session
-    return render_template('views/vehicle.html')
-
-
